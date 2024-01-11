52e49217deac12e56059881e4cba50db32251398
fabius lihanda <fabiuslihandaachevi@gmail.com>
2024-01-11 15:48:48 +0300

    ceasure update

--- Ceaser cipher.py
+++ Ceaser cipher.py
@@ -1,41 +1,37 @@
-def encrypt(plain_text, shift_amount):
-    encoded_result = ""
-    for char in text:
-        if char.isalpha():
+from art import logo
+print(logo)
+
+
+def ceaser(entry_text, shift_amount, cipher_direction):
+    end_result = ""
+    for char in entry_text:
+        if char.isalpha():  # or if char in alphabet
             index = alphabet.index(char.lower())
-            shifted_index = (index + shift) % 26
+            shifted_index = (index + shift_amount) % 26
+            if cipher_direction == "decode":
+                shifted_index = (index - shift_amount) % 26
             shifted_char = alphabet[shifted_index]
-            encoded_result += shifted_char
+            end_result += shifted_char
         else:
-            encoded_result += char  # Keep non-alphabetic characters unchanged
-
-    print(f"The encoded text is {encoded_result}")
-
-
-def decrypt(cypher_text, shift_amount):
-    decoded_result = ""
-    for char in text:
-        if char.isalpha():
-            index = alphabet.index(char.lower())
-            shifted_index = (index - shift) % 26
-            shifted_char = alphabet[shifted_index]
-            decoded_result += shifted_char
-        else:
-            decoded_result += char  # Keep non-alphabetic characters unchanged
-    print(f"The decoded text is {decoded_result}")
+            end_result += char  # Keep non-alphabetic characters unchanged
+    print(f"The {cipher_direction}d text is {end_result}")
 
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+repeat = "yes"
+while repeat.lower() == "yes":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 
-if direction == "encode" or direction == "decode":
-    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
-
-    if direction == "encode":
-        encrypt(text, shift)
+    if direction == "encode" or direction == "decode":
+        alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                    'u', 'v', 'w', 'x', 'y', 'z']  # adhere to pep style
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        ceaser(entry_text=text, shift_amount=shift, cipher_direction=direction)
     else:
-        decrypt(text, shift)
-else:
-    print("Invalid entry. Please type 'encode' or 'decode' to proceed.")
+        print("Invalid entry. Please type 'encode' or 'decode' to proceed.")
+    repeat = input("Type 'yes' if you want to go again. Otherwise type 'no':\n")
+
+if repeat.lower() != "yes":
+    print("Goodbye")
+
 
