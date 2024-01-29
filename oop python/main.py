72d9b17cf52d4b2b8ba9f7c23684590227bc2ac7
fabius lihanda <fabiuslihandaachevi@gmail.com>
2024-01-29 15:23:44 +0300

    object orinted programming

--- oop python/main.py
+++ oop python/main.py
@@ -9,5 +9,12 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-quiz =  QuizBrain(question_bank)
-quiz.next_question()
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print(
+    f"""You have completed the quiz
+    Your final score was {quiz.user_score}/{quiz.question_number} """
+)
+
