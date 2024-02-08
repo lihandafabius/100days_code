5e508e40757970f7f89702c4f08a5e65a4ff75ff
fabius lihanda <fabiuslihandaachevi@gmail.com>
2024-02-08 07:03:34 +0300

    Tkinter Module

---
+++ Tkinter Module/add.py
@@ -0,0 +1,6 @@
+def add(*args):
+    return sum(args)
+
+
+result = add(3, 4, 5, 7)
+print(result)
---
+++ Tkinter Module/mile_to_km.py
@@ -0,0 +1,9 @@
+from tkinter import *
+window = Tk()
+window.title("Miles to Km converter")
+window.minsize(width=500, height=300)
+
+button = Button(text="Calculate")
+button.pack()
+
+window.mainloop()
