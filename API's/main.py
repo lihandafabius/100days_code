406385525ab8ba00cc6f401807e3a1caeab0bac8
fabius lihanda <fabiuslihandaachevi@gmail.com>
2024-02-16 10:56:45 +0300

    Trivia Question App

---
+++ API's/main.py
@@ -0,0 +1,23 @@
+import requests
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+# data = response.json()
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+# iss_position = (longitude, latitude)
+# print(iss_position)
+MY_LAT = -0.303099
+MY_LONG = 36.080025
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
+print(sunrise.split("T")[1].split(":"))
+print(sunset)
