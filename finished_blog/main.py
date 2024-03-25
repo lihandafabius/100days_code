b664180c2255f5506bd7f4bc038c404f53023d55
fabius lihanda <fabiuslihandaachevi@gmail.com>
2024-03-25 21:09:46 +0300

    WTForms

--- finished_blog/main.py
+++ finished_blog/main.py
@@ -1,16 +1,13 @@
-import smtplib
-
 from flask import Flask, render_template, request
+import smtplib
 import requests
-
-my_email = "fabiuslihandaachevi@gmail.com"
-password = "msee ltmi vdyw eaye"
-
-# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
-posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 
 app = Flask(__name__)
 
+# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+OWN_EMAIL = "fabiuslihandaachevi@gmail.com"
+OWN_PASSWORD = "msee ltmi vdyw eaye"
 
 @app.route('/')
 def get_all_posts():
@@ -22,26 +19,20 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact", methods=["POST", "GET"])
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.form
-        print(data["name"])
-        print(data["email"])
-        print(data["phone"])
-        print(data["message"])
         send_email(data["name"], data["email"], data["phone"], data["message"])
         return render_template("contact.html", msg_sent=True)
     return render_template("contact.html", msg_sent=False)
 
-
 def send_email(name, email, phone, message):
     email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
-    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
-        connection.login(my_email, password)
-        connection.sendmail(my_email, password, email_message)
-
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 @app.route("/post/<int:index>")
 def show_post(index):
@@ -53,4 +44,4 @@ def show_post(index):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True)
