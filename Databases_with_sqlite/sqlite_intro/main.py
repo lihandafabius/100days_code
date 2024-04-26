cc86af595cadc5ea7fe4a18ef3db36658b032a3b
fabius lihanda <fabiuslihandaachevi@gmail.com>
2024-04-26 19:07:24 +0300

    updates

--- Databases_with_sqlite/sqlite_intro/main.py
+++ Databases_with_sqlite/sqlite_intro/main.py
@@ -8,39 +8,84 @@
 # cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
 # db.commit()
 
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy.orm import DeclarativeBase
+#
+# # Initialize Flask app
+# app = Flask(__name__)
+# # Configure the SQLite database, relative to the app instance folder
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+# # Disable tracking modifications for improved performance
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+#
+# # Initialize SQLAlchemy extension
+# db = SQLAlchemy(app)
+#
+# # Define the model for the books table
+#
+#
+# class Book(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(255), nullable=False)
+#     author = db.Column(db.String(255), nullable=False)
+#     rating = db.Column(db.Float, nullable=False)
+#
+#     def __repr__(self):
+#         return f"Book(id={self.id}, title={self.title}, author={self.author}, rating={self.rating})"
+#
+# # Create the table schema in the database
+#
+#
+# with app.app_context():
+#     db.create_all()
+#
+#     # Create a new entry in the books table
+#     new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
+#     db.session.add(new_book)
+#     db.session.commit()
+
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
-# Initialize Flask app
 app = Flask(__name__)
-# Configure the SQLite database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
-# Disable tracking modifications for improved performance
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-# Initialize SQLAlchemy extension
-db = SQLAlchemy(app)
-
-# Define the model for the books table
+#CREATE DATABASE
 
 
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
+
+# Create the extension
+db = SQLAlchemy(model_class=Base)
+# Initialise the app with the extension
+db.init_app(app)
+
+
+#CREATE TABLE
 class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
-    author = db.Column(db.String(255), nullable=False)
-    rating = db.Column(db.Float, nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
 
+    # Optional: this will allow each book object to be identified by its title when printed.
     def __repr__(self):
-        return f"Book(id={self.id}, title={self.title}, author={self.author}, rating={self.rating})"
-
-# Create the table schema in the database
+        return f'<Book {self.title}>'
 
 
+# Create table schema in the database. Requires application context.
 with app.app_context():
     db.create_all()
 
-    # Create a new entry in the books table
+# CREATE RECORD
+with app.app_context():
     new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
     db.session.add(new_book)
     db.session.commit()
