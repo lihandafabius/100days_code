7f54e8e77d549d47a84905faf1104f9d67aa5622
fabius lihanda <fabiuslihandaachevi@gmail.com>
2024-05-07 07:22:12 +0300

    sqlite with databases

--- Databases_with_sqlite/main.py
+++ Databases_with_sqlite/main.py
@@ -1,41 +1,85 @@
 from flask import Flask, render_template, request, redirect, url_for
-
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, mapped_column
+from sqlalchemy import Integer, String, Float
 
 app = Flask(__name__)
 
-all_books = []
+# CREATE DATABASE
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///books.db"
+# Create the extension
+db = SQLAlchemy(model_class=Base)
+# initialise the app with the extension
+db.init_app(app)
+
+# CREATE TABLE
+
+
+class Book(db.Model):
+    id = mapped_column(Integer, primary_key=True)
+    title = mapped_column(String(250), unique=True, nullable=False)
+    author = mapped_column(String(250), nullable=False)
+    rating = mapped_column(Float, nullable=False)
+
+
+# Create table schema in the database. Requires application context.
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
 def home():
-    return render_template("index.html", books=all_books)
+    with app.app_context():
+        all_books = db.session.query(Book).order_by(Book.title).all()
+        return render_template('index.html', all_books=all_books)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        new_book = {
-            "title": request.form["title"],
-            "author": request.form["author"],
-            "rating": request.form["rating"]
-        }
-        all_books.append(new_book)
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=float(request.form["rating"])
+        )
+        db.session.add(new_book)
+        db.session.commit()
         return redirect(url_for('home'))
     return render_template('add.html')
 
 
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        # UPDATE RECORD
+        book_id = request.form["id"]
+        book_to_update = db.get_or_404(Book, book_id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    book_id = request.args.get('id')
+    book_selected = db.get_or_404(Book, book_id)
+    return render_template("edit_rating.html", book=book_selected)
+
+
+@app.route("/delete")
+def delete():
+    book_id = request.args.get('id')
+
+    # DELETE A RECORD BY ID
+    book_to_delete = db.get_or_404(Book, book_id)
+    # Alternative way to select the book to delete.
+    # book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
--- Databases_with_sqlite/sqlite_intro/main.py
+++ Databases_with_sqlite/sqlite_intro/main.py
@@ -4,47 +4,9 @@
 # cursor = db.cursor()
 # cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, "
 #                "author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
-
+#
 # cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
 # db.commit()
-
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy.orm import DeclarativeBase
-#
-# # Initialize Flask app
-# app = Flask(__name__)
-# # Configure the SQLite database, relative to the app instance folder
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
-# # Disable tracking modifications for improved performance
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-#
-# # Initialize SQLAlchemy extension
-# db = SQLAlchemy(app)
-#
-# # Define the model for the books table
-#
-#
-# class Book(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(255), nullable=False)
-#     author = db.Column(db.String(255), nullable=False)
-#     rating = db.Column(db.Float, nullable=False)
-#
-#     def __repr__(self):
-#         return f"Book(id={self.id}, title={self.title}, author={self.author}, rating={self.rating})"
-#
-# # Create the table schema in the database
-#
-#
-# with app.app_context():
-#     db.create_all()
-#
-#     # Create a new entry in the books table
-#     new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
-#     db.session.add(new_book)
-#     db.session.commit()
-
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -53,7 +15,7 @@ from sqlalchemy import Integer, String, Float
 
 app = Flask(__name__)
 
-#CREATE DATABASE
+# CREATE DATABASE
 
 
 class Base(DeclarativeBase):
@@ -68,7 +30,7 @@ db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
 
-#CREATE TABLE
+# CREATE TABLE
 class Book(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
@@ -85,7 +47,34 @@ with app.app_context():
     db.create_all()
 
 # CREATE RECORD
+# with app.app_context():
+#     new_book = Book(title="The Elephant Dance ", author="HR Ole Kulet", rating=17)
+#     db.session.add(new_book)
+#     db.session.commit()
+
+# with app.app_context():
+#     result = db.session.execute(db.select(Book).order_by(Book.title))
+#     all_books = result.scalars()
+#     for book in all_books:
+#         print(book)
+
+book_id = 6
+# with app.app_context():
+#     book = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+#     print("Title:", book.title)
+#     print("Author:", book.author)
+#     print("Rating:", book.rating)
+#     print()  # Add an empty line for better readability)
+#
 with app.app_context():
-    new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
-    db.session.add(new_book)
+    book_to_update = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    # book_to_update.title = "The Elephant Dance"
+    # book_to_update.author = "HR Ole Kulet"
+    book_to_update.id = 5
     db.session.commit()
+
+# book_id = 8
+# with app.app_context():
+#     book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+#     db.session.delete(book_to_delete)
+#     db.session.commit()
